c626a7b9a059974ee5c37566d5f61d22c4c3423f
Daniel Bahrdt <daniel.bahrdt@eltako.de>
2021-04-14 17:02:47 +0200

    [Espressif] esp32_mp: split wait_until_done

--- wolfcrypt/src/port/Espressif/esp32_mp.c
+++ wolfcrypt/src/port/Espressif/esp32_mp.c
@@ -148,6 +148,12 @@ static void process_start(word32 reg)
     /* start process  */
     DPORT_REG_WRITE(reg, 1);
 }
+
+static int wolfssl_esp_dport_reg_read(word32 reg) {
+	return DPORT_REG_READ(reg);
+// 	return esp_dport_access_reg_read(reg);
+}
+
 /* wait until done */
 static int wait_uitil_done(word32 reg)
 {
@@ -157,6 +163,13 @@ static int wait_uitil_done(word32 reg)
         if(++timeout < ESP_RSA_TIMEOUT && DPORT_REG_READ(reg) == 1){
             break;
         }
+// 		if (timeout >= ESP_RSA_TIMEOUT) {
+// 			break;
+// 		}
+//         if (wolfssl_esp_dport_reg_read(reg) == 1) {
+//             break;
+//         }
+// 		++timeout;
     }
 
     /* clear interrupt */
